daad2b17a94573f6e898b99ac1662d27c7ceaaa3
sibilla <sibilla.merlo@di.unito.it>
2020-12-01 19:09:04 +0100

    entropia e luminanza pesate

--- code_merlo/main_entropy.m
+++ code_merlo/main_entropy.m
@@ -8,8 +8,11 @@ luminance_image_vector = rgb_to_luminance (image_vector);
 %calculate entropy images from luminance and save them in a vector
 entropy_image_vector = entropy_images(luminance_image_vector);
 
+%weight 
+W_normalized = W(entropy_image_vector, luminance_image_vector);
+
 %creation of hdr image
-ScaryImage = hdr(entropy_image_vector, image_vector);
+ScaryImage = hdr(W_normalized, image_vector); 
 
 figure('Name', 'Result'); imshow(ScaryImage);
 
